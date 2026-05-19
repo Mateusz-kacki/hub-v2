4bd6ba0fc90fbf5629b38326d995c7810c8d66a7
Mateusz-kacki <nnax2607@gmail.com>
2026-05-19 13:14:55 +0200

    Create layout.py

---
+++ backend/layout.py
@@ -0,0 +1,34 @@
+GRIDS = [
+    {
+        "id": "dock16-left",
+        "dock": 16,
+        "rows": 8
+    },
+    {
+        "id": "dock16-right",
+        "dock": 16,
+        "rows": 8
+    },
+
+    {
+        "id": "dock17-left",
+        "dock": 17,
+        "rows": 8
+    },
+    {
+        "id": "dock17-right",
+        "dock": 17,
+        "rows": 8
+    },
+
+    {
+        "id": "dock18-left",
+        "dock": 18,
+        "rows": 8
+    },
+    {
+        "id": "dock18-right",
+        "dock": 18,
+        "rows": 8
+    }
+]
