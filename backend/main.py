da57820675de365351b4faaec86675f19eee21f7
Mateusz-kacki <nnax2607@gmail.com>
2026-05-19 13:19:20 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 from excel_routes import router as excel_router
+from layout import GRIDS
 
 app = FastAPI(title="Warehouse Planner API")
 
 app.include_router(excel_router)
+
 
 @app.get("/")
 def root():
     return {
         "status": "OK",
         "message": "Warehouse Planner backend is running"
+    }
+
+
+@app.get("/layout")
+def get_layout():
+    return {
+        "grids": GRIDS
     }
