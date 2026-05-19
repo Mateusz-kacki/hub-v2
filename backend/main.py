3460246502244bf6964dacd44c0dd1bd4c9f4419
Mateusz-kacki <nnax2607@gmail.com>
2026-05-19 12:15:46 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from excel_routes import router as excel_router
 
 app = FastAPI(title="Warehouse Planner API")
+
+app.include_router(excel_router)
 
 @app.get("/")
 def root():
