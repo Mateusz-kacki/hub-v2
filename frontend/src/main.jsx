5ecf509c8c0eaed9c5b4ffe6f168b9f413544338
Mateusz-kacki <nnax2607@gmail.com>
2026-05-19 13:23:08 +0200

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Warehouse Planner</h1>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
