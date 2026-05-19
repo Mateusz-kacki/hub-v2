bcb8d1f8614bc5d2dd3aa4a0207564b745f7691a
Mateusz-kacki <nnax2607@gmail.com>
2026-05-19 13:18:22 +0200

    Update layout.py

--- backend/layout.py
+++ backend/layout.py
@@ -2,33 +2,84 @@ GRIDS = [
     {
         "id": "dock16-left",
         "dock": 16,
-        "rows": 8
+
+        "x": 10,
+        "y": 20,
+
+        "width": 12,
+        "height": 25,
+
+        "rows": 8,
+        "columns": 8
     },
+
     {
         "id": "dock16-right",
         "dock": 16,
-        "rows": 8
+
+        "x": 24,
+        "y": 20,
+
+        "width": 12,
+        "height": 25,
+
+        "rows": 8,
+        "columns": 8
     },
 
     {
         "id": "dock17-left",
         "dock": 17,
-        "rows": 8
+
+        "x": 40,
+        "y": 20,
+
+        "width": 12,
+        "height": 25,
+
+        "rows": 8,
+        "columns": 8
     },
+
     {
         "id": "dock17-right",
         "dock": 17,
-        "rows": 8
+
+        "x": 54,
+        "y": 20,
+
+        "width": 12,
+        "height": 25,
+
+        "rows": 8,
+        "columns": 8
     },
 
     {
         "id": "dock18-left",
         "dock": 18,
-        "rows": 8
+
+        "x": 70,
+        "y": 20,
+
+        "width": 12,
+        "height": 25,
+
+        "rows": 8,
+        "columns": 8
     },
+
     {
         "id": "dock18-right",
         "dock": 18,
-        "rows": 8
+
+        "x": 84,
+        "y": 20,
+
+        "width": 12,
+        "height": 25,
+
+        "rows": 8,
+        "columns": 8
     }
 ]
