d2553bdf36c67692455bdf6d2e5834854ab0d42a
Mateusz-kacki <nnax2607@gmail.com>
2026-05-26 12:03:35 +0200

    Update layout.py

--- backend/layout.py
+++ backend/layout.py
@@ -1,85 +1,103 @@
-GRIDS = [
+DOCK_PRIORITY = [16, 17, 18]
+
+AISLES = [
     {
-        "id": "dock16-left",
-        "dock": 16,
-
-        "x": 10,
-        "y": 20,
-
-        "width": 12,
-        "height": 25,
-
-        "rows": 8,
-        "columns": 8
+        "id": "S16-2",
+        "docks": [18],
+        "capacityUnits": 16,
+        "maxStores": 2,
+        "x": 48,
+        "y": 8,
+        "width": 28,
+        "height": 4,
+        "priority": 1
     },
-
     {
-        "id": "dock16-right",
-        "dock": 16,
-
-        "x": 24,
-        "y": 20,
-
-        "width": 12,
-        "height": 25,
-
-        "rows": 8,
-        "columns": 8
+        "id": "S16-6",
+        "docks": [17, 18],
+        "capacityUnits": 16,
+        "maxStores": 2,
+        "x": 48,
+        "y": 12,
+        "width": 28,
+        "height": 4,
+        "priority": 2
     },
-
     {
-        "id": "dock17-left",
-        "dock": 17,
-
-        "x": 40,
-        "y": 20,
-
-        "width": 12,
-        "height": 25,
-
-        "rows": 8,
-        "columns": 8
+        "id": "S16-7",
+        "docks": [17],
+        "capacityUnits": 16,
+        "maxStores": 2,
+        "x": 48,
+        "y": 16,
+        "width": 28,
+        "height": 4,
+        "priority": 3
     },
-
     {
-        "id": "dock17-right",
-        "dock": 17,
-
-        "x": 54,
+        "id": "S16-8",
+        "docks": [16, 17],
+        "capacityUnits": 16,
+        "maxStores": 2,
+        "x": 48,
         "y": 20,
-
-        "width": 12,
-        "height": 25,
-
-        "rows": 8,
-        "columns": 8
+        "width": 28,
+        "height": 4,
+        "priority": 4
     },
-
     {
-        "id": "dock18-left",
-        "dock": 18,
-
-        "x": 70,
-        "y": 20,
-
-        "width": 12,
-        "height": 25,
-
-        "rows": 8,
-        "columns": 8
+        "id": "S16-9",
+        "docks": [16, 17],
+        "capacityUnits": 16,
+        "maxStores": 2,
+        "x": 48,
+        "y": 24,
+        "width": 28,
+        "height": 4,
+        "priority": 5
     },
-
     {
-        "id": "dock18-right",
-        "dock": 18,
-
-        "x": 84,
-        "y": 20,
-
-        "width": 12,
-        "height": 25,
-
-        "rows": 8,
-        "columns": 8
+        "id": "S17-8",
+        "docks": [16],
+        "capacityUnits": 16,
+        "maxStores": 2,
+        "x": 48,
+        "y": 36,
+        "width": 28,
+        "height": 4,
+        "priority": 6
+    },
+    {
+        "id": "S17-9",
+        "docks": [16],
+        "capacityUnits": 16,
+        "maxStores": 2,
+        "x": 48,
+        "y": 40,
+        "width": 28,
+        "height": 4,
+        "priority": 7
+    },
+    {
+        "id": "S17-10",
+        "docks": [16],
+        "capacityUnits": 16,
+        "maxStores": 2,
+        "x": 48,
+        "y": 44,
+        "width": 28,
+        "height": 4,
+        "priority": 8
+    },
+    {
+        "id": "S17-11",
+        "docks": [16],
+        "capacityUnits": 16,
+        "maxStores": 2,
+        "x": 48,
+        "y": 48,
+        "width": 28,
+        "height": 4,
+        "priority": 9
     }
 ]
